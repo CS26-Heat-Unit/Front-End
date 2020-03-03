a5264c334555cfd2b6eed65a16e6842b7ccd1a7c
Emily_Richard <richard.emily.e@gmail.com>
2020-03-02 23:30:56 -0500

    basic formatting, added components, set up folders for redux, lay basic structs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import Header from "./components/Header";
+import Console from "./components/Console";
 
 function App() {
   return (
-    <div>
-      <h1>Heat Unit</h1>
+    <div className="App">
+      <Header />
+      <Console />
     </div>
   );
 }
---
+++ src/components/Score.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Score() {
+  return (
+    <div className="score">
+      hello from inside the score
+    </div>
+  );
+}
+
+export default Score;
