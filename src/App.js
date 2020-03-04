b9c6ce106d0097dd46911767d40392786eb4201a
Bayron Puac <bayronpuac@gmail.com>
2020-03-04 15:34:39 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import Header from "./components/Header";
 import Console from "./components/Console";
+import Login from "./components/Login";
+import Map from "./components/Map"
 
 function App() {
   return (
     <div className="App">
+      <Login />
       <Header />
       <Console />
+      <Map />
     </div>
   );
 }
